dcbc2624ca3af6650bd4165965967e9cadad8ab6
Matias Bengtsson <bengtsson1985@hotmail.com>
2024-09-03 20:34:09 +0200

    kodtest utmaning

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-﻿namespace InterperterNaviPro;
+﻿using System.Data;
+
+namespace InterperterNaviPro;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+  
+        string expression = "1+ (36 / 2) / (3 * 3) - 2";
+
+        string result = EvaluateExpression(expression);
+
+        Console.WriteLine("Epression " + expression);
+        Console.WriteLine("result " + result);
+        Console.ReadLine();
     }
+
 }
